cbb75c305de156115811e4af867037c9783e3a00
frans <zxc@qq.com>
2020-08-03 15:06:56 +0800

    init

---
+++ app/controller/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controller;
+
+use support\Request;
+
+/**
+ * 控制器基类
+ * Class Controller
+ * @package app\controller
+ */
+class Controller
+{
+    public function __construct()
+    {
+    }
+
+}
